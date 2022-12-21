51e830450199ff675ba819d1cbaf4c779f001d5f
Peyton Hobson <61252072+peytonhobson@users.noreply.github.com>
2022-12-20 17:40:50 -0700

    Fixed saving bank account not deleting card (#35)

--- server/api/stripe-payment-methods.js
+++ server/api/stripe-payment-methods.js
@@ -3,14 +3,10 @@ const { handleError, serialize } = require('../api-util/sdk');
 const log = require('../log');
 
 module.exports = (req, res) => {
-  // Create a PaymentIntent with the order amount and currency
-
   const { stripeCustomerId } = req.body;
 
   return stripe.customers
-    .listPaymentMethods(stripeCustomerId, {
-      type: 'card',
-    })
+    .listPaymentMethods(stripeCustomerId)
     .then(apiResponse => {
       res
         .status(200)
