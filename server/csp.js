0e9549a7b843e9aee7eb674205a3013ca75723b9
peytonhobson <peyton.hobson1@gmail.com>
2023-03-15 16:51:50 -0600

    Changed csp

--- server/csp.js
+++ server/csp.js
@@ -36,7 +36,7 @@ const defaultDirectives = {
     '*.google-analytics.com',
     'stats.g.doubleclick.net',
 
-    '*.sentry.io',
+    'sentry.io',
     '*.stripe.com',
   ],
   fontSrc: [self, data, 'assets-sharetribecom.sharetribe.com', 'fonts.gstatic.com'],
@@ -104,7 +104,11 @@ module.exports = (reportUri, enforceSsl, reportOnly) => {
   // const { imgSrc = [self] } = defaultDirectives;
   // const exampleImgSrc = imgSrc.concat('my-custom-domain.example.com');
   const { connectSrc = [self], imgSrc = [self] } = defaultDirectives;
-  const customConnectSrc = connectSrc.concat('*.sendbird.com').concat('wss://*.sendbird.com');
+  const customConnectSrc = connectSrc
+    .concat('*.sendbird.com')
+    .concat('wss://*.sendbird.com')
+    .concat('*.sentry.io')
+    .concat('*.ingest.sentry.io');
   const customImgSrc = [self, data, blob, ...devImagesMaybe, 'https:'];
 
   const customDirectives = {
