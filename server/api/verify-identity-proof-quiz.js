c37f5e62404f44c90a99c9877ba8039b4e3931ca
peytonhobson <peyton.hobson1@gmail.com>
2023-03-16 06:11:35 -0600

    Changed identity proof quiz to success

--- server/api/verify-identity-proof-quiz.js
+++ server/api/verify-identity-proof-quiz.js
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
   const mockString = isDev ? 'mock/' : '';
 
   const userAccessCodeMaybe = {
-    userAccessCode: isDev ? mockUserAccessCodes[2] : payload.userAccessCode,
+    userAccessCode: isDev ? mockUserAccessCodes[1] : payload.userAccessCode,
   };
 
   axios
