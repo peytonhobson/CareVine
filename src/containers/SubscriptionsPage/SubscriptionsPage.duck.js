ffa30faa46df3e4f4fac0da849de6ebe691d4651
peytonhobson <peyton.hobson1@gmail.com>
2023-04-04 07:48:24 -0600

    Changed to update subscription item

--- src/containers/SubscriptionsPage/SubscriptionsPage.duck.js
+++ src/containers/SubscriptionsPage/SubscriptionsPage.duck.js
@@ -1,28 +1,31 @@
 import { fetchCurrentUser } from '../../ducks/user.duck';
-import { setInitialValues as setInitialValuesForPaymentMethods } from '../../ducks/paymentMethods.duck';
+import {
+  setInitialValues as setInitialValuesForPaymentMethods,
+  fetchDefaultPayment,
+} from '../../ducks/paymentMethods.duck';
 import { storableError } from '../../util/errors';
-import { stripePaymentMethods } from '../../util/api';
+import { stripePaymentMethods, stripeUpdateSubscriptionItem } from '../../util/api';
 import * as log from '../../util/log';
 
 // ================ Action types ================ //
 
-export const STRIPE_CUSTOMER_REQUEST = 'app/PaymentMethodsPage/STRIPE_CUSTOMER_REQUEST';
-export const STRIPE_CUSTOMER_SUCCESS = 'app/PaymentMethodsPage/STRIPE_CUSTOMER_SUCCESS';
-export const STRIPE_CUSTOMER_ERROR = 'app/PaymentMethodsPage/STRIPE_CUSTOMER_ERROR';
+export const STRIPE_CUSTOMER_REQUEST = 'app/SubscriptionsPage/STRIPE_CUSTOMER_REQUEST';
+export const STRIPE_CUSTOMER_SUCCESS = 'app/SubscriptionsPage/STRIPE_CUSTOMER_SUCCESS';
+export const STRIPE_CUSTOMER_ERROR = 'app/SubscriptionsPage/STRIPE_CUSTOMER_ERROR';
 
-export const FETCH_DEFAULT_PAYMENT_REQUEST = 'app/PaymentMethodsPage/FETCH_DEFAULT_PAYMENT_REQUEST';
-export const FETCH_DEFAULT_PAYMENT_SUCCESS = 'app/PaymentMethodsPage/FETCH_DEFAULT_PAYMENT_SUCCESS';
-export const FETCH_DEFAULT_PAYMENT_ERROR = 'app/PaymentMethodsPage/FETCH_DEFAULT_PAYMENT_ERROR';
-export const DEFAULT_PAYMENT_FETCHED = 'app/PaymentMethodsPage/DEFAULT_PAYMENT_FETCHED';
+export const UPDATE_SUBSCRIPTION_ITEM_REQUEST =
+  'app/PaymentMethodsPage/UPDATE_SUBSCRIPTION_ITEM_REQUEST';
+export const UPDATE_SUBSCRIPTION_ITEM_SUCCESS =
+  'app/PaymentMethodsPage/UPDATE_SUBSCRIPTION_ITEM_SUCCESS';
+export const UPDATE_SUBSCRIPTION_ITEM_ERROR =
+  'app/PaymentMethodsPage/UPDATE_SUBSCRIPTION_ITEM_ERROR';
 
 // ================ Reducer ================ //
 
 const initialState = {
   stripeCustomerFetched: false,
-  defaultPaymentMethods: null,
-  defaultPaymentFetched: false,
-  fetchDefaultPaymentError: null,
-  fetchDefaultPaymentInProgress: false,
+  updateSubscriptionItemInProgress: false,
+  updateSubscriptionItemError: null,
 };
 
 export default function payoutMethodsPageReducer(state = initialState, action = {}) {
@@ -36,29 +39,20 @@ export default function payoutMethodsPageReducer(state = initialState, action = 
       console.error(payload); // eslint-disable-line no-console
       return { ...state, stripeCustomerFetchError: payload };
 
-    case FETCH_DEFAULT_PAYMENT_REQUEST:
-      return { ...state, fetchDefaultPaymentInProgress: true, fetchDefaultPaymentError: null };
-    case FETCH_DEFAULT_PAYMENT_SUCCESS:
-      const card = payload.find(p => p.type === 'card');
-      const bankAccount = payload.find(p => p.type === 'us_bank_account');
+    case UPDATE_SUBSCRIPTION_ITEM_REQUEST:
       return {
         ...state,
-        fetchDefaultPaymentInProgress: false,
-        defaultPaymentMethods: {
-          card,
-          bankAccount,
-        },
-        defaultPaymentFetched: true,
+        updateSubscriptionItemInProgress: true,
+        updateSubscriptionItemError: null,
       };
-    case FETCH_DEFAULT_PAYMENT_ERROR:
+    case UPDATE_SUBSCRIPTION_ITEM_SUCCESS:
+      return { ...state, updateSubscriptionItemInProgress: false };
+    case UPDATE_SUBSCRIPTION_ITEM_ERROR:
       return {
         ...state,
-        fetchDefaultPaymentInProgress: false,
-        fetchDefaultPaymentError: payload,
-        defaultPaymentFetched: true,
+        updateSubscriptionItemInProgress: false,
+        updateSubscriptionItemError: payload,
       };
-    case DEFAULT_PAYMENT_FETCHED:
-      return { ...state, defaultPaymentFetched: true };
 
     default:
       return state;
@@ -75,17 +69,13 @@ export const stripeCustomerError = e => ({
   payload: e,
 });
 
-export const fetchDefaultPaymentRequest = () => ({ type: FETCH_DEFAULT_PAYMENT_REQUEST });
-export const fetchDefaultPaymentSuccess = defaultPayment => ({
-  type: FETCH_DEFAULT_PAYMENT_SUCCESS,
-  payload: defaultPayment,
-});
-export const fetchDefaultPaymentError = e => ({
-  type: FETCH_DEFAULT_PAYMENT_ERROR,
+export const updateSubscriptionItemRequest = () => ({ type: UPDATE_SUBSCRIPTION_ITEM_REQUEST });
+export const updateSubscriptionItemSuccess = () => ({ type: UPDATE_SUBSCRIPTION_ITEM_SUCCESS });
+export const updateSubscriptionItemError = e => ({
+  type: UPDATE_SUBSCRIPTION_ITEM_ERROR,
   error: true,
   payload: e,
 });
-export const defaultPaymentFetched = () => ({ type: DEFAULT_PAYMENT_FETCHED });
 
 // ================ Thunks ================ //
 
@@ -103,25 +93,6 @@ export const stripeCustomer = () => (dispatch, getState, sdk) => {
     });
 };
 
-export const fetchDefaultPayment = stripeCustomerId => (dispatch, getState, sdk) => {
-  dispatch(fetchDefaultPaymentRequest());
-
-  const handleSuccess = response => {
-    dispatch(fetchDefaultPaymentSuccess(response.data.data));
-    return response;
-  };
-
-  const handleError = e => {
-    dispatch(fetchDefaultPaymentError(storableError(e)));
-    log.error(e, 'fetch-default-payment-failed', {});
-    throw e;
-  };
-
-  return stripePaymentMethods({ stripeCustomerId })
-    .then(handleSuccess)
-    .catch(handleError);
-};
-
 export const loadData = () => (dispatch, getState, sdk) => {
   dispatch(setInitialValuesForPaymentMethods());
 
@@ -130,8 +101,26 @@ export const loadData = () => (dispatch, getState, sdk) => {
     dispatch(fetchCurrentUser());
     if (stripeCustomer) {
       return dispatch(fetchDefaultPayment(stripeCustomer.attributes.stripeCustomerId));
-    } else {
-      return dispatch(defaultPaymentFetched());
     }
   });
+};
+
+export const updateSubscriptionItem = (subscriptionId, priceId) => async (
+  dispatch,
+  getState,
+  sdk
+) => {
+  dispatch(updateSubscriptionItemRequest());
+
+  try {
+    const response = await stripeUpdateSubscriptionItem({
+      subscriptionId,
+      params: { price: priceId },
+    });
+
+    dispatch(updateSubscriptionItemSuccess());
+  } catch (e) {
+    log.error(e, 'update-subscription-item-failed');
+    dispatch(updateSubscriptionItemError(e));
+  }
 };
