6b817344e2fef8158f68815752b9f2a946277ac8
peytonhobson <peyton.hobson1@gmail.com>
2023-04-19 14:21:32 -0600

    Added no conversations icon

--- src/components/InboxItem/InboxItem.js
+++ src/components/InboxItem/InboxItem.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NamedLink, Avatar, UserDisplayName } from '../';
 import { propTypes } from '../../util/types';
 import { oneOf } from 'prop-types';
 import { intlShape } from '../../util/reactIntl';
 import { txIsRequested } from '../../util/transaction';
 import { createSlug, stringify } from '../../util/urlHelpers';
-import { IconVerticalDots } from '../../components';
+import {
+  IconVerticalDots,
+  Menu,
+  MenuContent,
+  MenuLabel,
+  MenuItem,
+  InlineTextButton,
+} from '../../components';
 import { isToday, isYesterday, timestampToDate } from '../../util/dates';
 import moment from 'moment';
 import classNames from 'classnames';
 
 import css from './InboxItem.module.css';
+
+const MENU_CONTENT_OFFSET = -12;
 
 const formatPreviewDate = createdAt => {
   if (isToday(createdAt)) {
@@ -48,6 +57,8 @@ const truncateString = function(fullStr, strLen) {
 };
 
 const InboxItem = props => {
+  const [isDeleteMenuOpen, setIsDeleteMenuOpen] = useState(false);
+
   const {
     tx,
     intl,
@@ -65,9 +76,6 @@ const InboxItem = props => {
   const otherUserDisplayName = <UserDisplayName user={otherUser} intl={intl} />;
   const isOtherUserBanned = otherUser?.attributes?.banned;
 
-  const isSaleNotification = txIsRequested(tx);
-  const rowNotificationDot = isSaleNotification ? <div className={css.notificationDot} /> : null;
-
   const notificationDot = false ? <div className={css.notificationDot} /> : null;
 
   const title = otherUserDisplayName;
@@ -75,10 +83,11 @@ const InboxItem = props => {
   const text = truncateString(previewMessage, 40);
 
   const activeClass = isActive ? css.active : null;
+  const deleteOpenClass = isDeleteMenuOpen ? css.deleteOpen : null;
 
   return (
     <div
-      className={classNames(css.inboxPreview, activeClass)}
+      className={classNames(css.inboxPreview, deleteOpenClass, activeClass)}
       key={id}
       onClick={() => onPreviewClick(id)}
     >
@@ -94,18 +103,36 @@ const InboxItem = props => {
         <div className={css.inboxPreviewLower}>
           <div className={css.inboxText}>{text}</div>
         </div>
-        <div
-          className={css.inboxPreviewAction}
-          onClick={e => {
-            e.stopPropagation();
-            onOpenDeleteConversationModal(id);
-          }}
-        >
-          <IconVerticalDots
-            height={isMobile ? '1.5em' : '0.75em'}
-            width={isMobile ? '1.75em' : '1em'}
-            className={css.menuIcon}
-          />
+        <div className={css.inboxPreviewAction}>
+          <Menu
+            className={classNames(css.menu, { [css.cardIsOpen]: !isDeleteMenuOpen })}
+            contentPlacementOffset={MENU_CONTENT_OFFSET}
+            contentPosition="left"
+            useArrow={false}
+            onToggleActive={() => setIsDeleteMenuOpen(prev => !prev)}
+            isOpen={isDeleteMenuOpen}
+          >
+            <MenuLabel className={css.menuLabel} isOpenClassName={css.listingMenuIsOpen}>
+              <IconVerticalDots
+                height={isMobile ? '1.5em' : '0.75em'}
+                width={isMobile ? '1.75em' : '1em'}
+                className={css.menuIcon}
+              />
+            </MenuLabel>
+            <MenuContent rootClassName={css.menuContent}>
+              <MenuItem key="delete-conversation" className={css.menuItem}>
+                <InlineTextButton
+                  className={css.deleteButton}
+                  onClick={e => {
+                    e.stopPropagation();
+                    onOpenDeleteConversationModal(id);
+                  }}
+                >
+                  Delete Conversation
+                </InlineTextButton>
+              </MenuItem>
+            </MenuContent>
+          </Menu>
         </div>
       </div>
     </div>
