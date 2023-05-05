1962f5b17d1a1b30a679ae7610fe90b19c012394
peytonhobson <peyton.hobson1@gmail.com>
2023-05-05 10:15:18 -0600

    Created blog post page

--- src/containers/BlogPostPage/BlogPostPage.js
+++ src/containers/BlogPostPage/BlogPostPage.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import CardGrid from './CardGrid';
 import { isScrollingDisabled } from '../../ducks/UI.duck';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
@@ -10,29 +7,9 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Page, LayoutSingleColumn, LayoutWrapperTopbar, LayoutWrapperMain } from '../../components';
 import { TopbarContainer } from '..';
 
-import css from './BlogHomePage.module.css';
+import css from './BlogPostPage.module.css';
 
 const STRAPI_API_URL = `${process.env.REACT_APP_STRAPI_URL}/graphql`;
-
-const useStyles = makeStyles(theme => ({
-  hero: {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
-    height: '500px',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#fff',
-    fontSize: '4rem',
-    [theme.breakpoints.down('sm')]: {
-      height: 300,
-      fontSize: '3em',
-    },
-  },
-}));
 
 const client = new ApolloClient({
   uri: STRAPI_API_URL,
@@ -41,11 +18,10 @@ const client = new ApolloClient({
 
 const BlogHomePageComponent = props => {
   const { scrollingDisabled } = props;
-  const classes = useStyles();
 
   // TODO: Update these for SEO
-  const schemaTitle = 'Blog';
-  const schemaDescription = 'Blog';
+  const schemaTitle = 'Blog Post';
+  const schemaDescription = 'Blog Post';
 
   return (
     <Page
@@ -66,12 +42,7 @@ const BlogHomePageComponent = props => {
           <TopbarContainer currentPage="BlogHomePage" />
         </LayoutWrapperTopbar>
         <LayoutWrapperMain className={css.mainWrapper}>
-          <Box className={classes.hero}>
-            <Box>Blog</Box>
-          </Box>
-          <ApolloProvider client={client}>
-            <CardGrid />
-          </ApolloProvider>
+          <ApolloProvider client={client}></ApolloProvider>
         </LayoutWrapperMain>
       </LayoutSingleColumn>
     </Page>
@@ -84,10 +55,6 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  onSendFeedbackEmail: feedback => dispatch(feedbackEmail(feedback)),
-});
+const BlogPostPage = compose(connect(mapStateToProps))(BlogHomePageComponent);
 
-const BlogHomePage = compose(connect(mapStateToProps))(BlogHomePageComponent);
-
-export default BlogHomePage;
+export default BlogPostPage;
