6c4dcaec79121107d081ce50d355b5b408868994
peytonhobson <peyton.hobson1@gmail.com>
2023-05-04 19:55:58 -0600

    Got working graphQL query

--- src/containers/BlogHomePage/BlogHomePage.js
+++ src/containers/BlogHomePage/BlogHomePage.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import Pagination from '@material-ui/lab/Pagination';
+import CardGrid from './CardGrid';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import { Page, LayoutSingleColumn, LayoutWrapperTopbar, LayoutWrapperMain } from '../../components';
+import { TopbarContainer } from '..';
+
+import css from './BlogHomePage.module.css';
 
 const useStyles = makeStyles(theme => ({
-  appBar: {
-    backgroundColor: '#fff',
-  },
   hero: {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
     height: '500px',
@@ -36,204 +30,73 @@ const useStyles = makeStyles(theme => ({
       fontSize: '3em',
     },
   },
-  blogsContainer: {
-    paddingTop: theme.spacing(3),
-  },
-  blogTitle: {
-    fontWeight: 800,
-    paddingBottom: theme.spacing(3),
-  },
-  card: {
-    maxWidth: '100%',
-  },
-  media: {
-    height: 240,
-  },
-  cardActions: {
-    display: 'flex',
-    margin: '0 10px',
-    justifyContent: 'space-between',
-  },
-  author: {
-    display: 'flex',
-  },
-  paginationContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
 }));
 
-const BlogHomePage = props => {
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache(),
+});
+
+const BlogHomePageComponent = props => {
+  const { scrollingDisabled } = props;
   const classes = useStyles();
 
+  // TODO: Update these for SEO
+  const schemaTitle = 'Blog';
+  const schemaDescription = 'Blog';
+
   return (
-    <div className="App">
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-          <Typography variant="h6" color="primary">
-            Blog
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box className={classes.hero}>
-        <Box>React Blog</Box>
-      </Box>
-      <Container maxWidth="lg" className={classes.blogsContainer}>
-        <Typography variant="h4" className={classes.blogTitle}>
-          Articles
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React useContext
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React Router
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React useContext
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React useContext
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
-        </Grid>
-        <Box my={4} className={classes.paginationContainer}>
-          <Pagination count={10} />
-        </Box>
-      </Container>
-    </div>
+    <Page
+      className={css.root}
+      scrollingDisabled={scrollingDisabled}
+      contentType="website"
+      description={schemaDescription}
+      title={schemaTitle}
+      schema={{
+        '@context': 'http://schema.org',
+        '@type': 'WebPage',
+        description: schemaDescription,
+        name: schemaTitle,
+      }}
+    >
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer currentPage="BlogHomePage" />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperMain className={css.mainWrapper}>
+          <Box className={classes.hero}>
+            <Box>Blog</Box>
+          </Box>
+          <ApolloProvider client={client}>
+            <CardGrid />
+          </ApolloProvider>
+        </LayoutWrapperMain>
+      </LayoutSingleColumn>
+    </Page>
   );
 };
+
+const mapStateToProps = state => {
+  const { currentUser } = state.user;
+  const {
+    sendFeedbackEmailInProgress,
+    sendFeedbackEmailError,
+    sendFeedbackEmailSuccess,
+  } = state.FeedbackPage;
+
+  return {
+    scrollingDisabled: isScrollingDisabled(state),
+    currentUser,
+    sendFeedbackEmailInProgress,
+    sendFeedbackEmailError,
+    sendFeedbackEmailSuccess,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSendFeedbackEmail: feedback => dispatch(feedbackEmail(feedback)),
+});
+
+const BlogHomePage = compose(connect(mapStateToProps, mapDispatchToProps))(BlogHomePageComponent);
 
 export default BlogHomePage;
