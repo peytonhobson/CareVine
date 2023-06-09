4d0a8c5b8a296464a2143eb8b214b54851287757
peytonhobson <peyton.hobson1@gmail.com>
2023-06-09 14:34:07 -0600

    Set up booking modal for mobile

--- src/containers/ListingPage/BookingContainer.js
+++ src/containers/ListingPage/BookingContainer.js
@@ -3,12 +3,20 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 import { useMediaQuery } from '@mui/material';
 import { InitialBookingForm } from '../../forms';
-import { Button } from '../../components';
+import { Button, Modal, Avatar } from '../../components';
 
 import css from './ListingPage.module.css';
 
 const BookingContainer = props => {
-  const { listing, onSubmit, monthlyTimeSlots } = props;
+  const {
+    listing,
+    onSubmit,
+    monthlyTimeSlots,
+    onManageDisableScrolling,
+    authorDisplayName,
+  } = props;
+
+  const [isBookingModalOpen, setIsBookingModalOpen] = useState(false);
 
   const isLarge = useMediaQuery('(min-width:1024px)');
   const minPrice = listing.attributes.publicData.minPrice;
@@ -30,8 +38,30 @@ const BookingContainer = props => {
           <p className={css.startingRateText}>Starting Rate</p>
           <p className={css.startingRate}>${minPrice / 100}</p>
         </div>
-        <Button className={css.availabilityButton}>Check Availability</Button>
+        <Button className={css.availabilityButton} onClick={() => setIsBookingModalOpen(true)}>
+          Check Availability
+        </Button>
       </div>
+      <Modal
+        className={css.bookingModal}
+        isOpen={isBookingModalOpen}
+        onClose={() => setIsBookingModalOpen(false)}
+        onManageDisableScrolling={onManageDisableScrolling}
+        usePortal
+        id="BookingModal"
+      >
+        <div className={css.bookingModalHeader}>
+          <h1 className={css.modalTitle}>Book {authorDisplayName}</h1>
+          <Avatar className={css.bookingAvatar} user={listing.author} />
+        </div>
+        <InitialBookingForm
+          className={css.bookingForm}
+          listing={listing}
+          onSubmit={onSubmit}
+          monthlyTimeSlots={monthlyTimeSlots}
+          // inProgress={bookingInProgress}
+        />
+      </Modal>
     </>
   );
 };
