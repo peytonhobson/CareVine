48a8209beea972fae67c2480c2547d152bfadd42
peytonhobson <peyton.hobson1@gmail.com>
2023-07-27 17:17:17 -0600

    Refactored booking summary card

---
+++ src/components/BookingSummaryCard/BookingItems.js
@@ -0,0 +1,161 @@
+import React from 'react';
+
+import { convertTimeFrom12to24 } from '../../util/data';
+import moment from 'moment';
+import { addTimeToStartOfDay } from '../../util/dates';
+
+import css from './BookingSummaryCard.module.css';
+
+const weekdayMap = {
+  sun: 0,
+  mon: 1,
+  tue: 2,
+  wed: 3,
+  thu: 4,
+  fri: 5,
+  sat: 6,
+};
+
+const calculateTimeBetween = (bookingStart, bookingEnd) => {
+  const start = convertTimeFrom12to24(bookingStart).split(':')[0];
+  const end = bookingEnd === '12:00am' ? 24 : convertTimeFrom12to24(bookingEnd).split(':')[0];
+
+  return end - start;
+};
+
+const calculateCost = (bookingStart, bookingEnd, price) =>
+  parseFloat(calculateTimeBetween(bookingStart, bookingEnd) * Number(price)).toFixed(2);
+
+const BookingItem = props => {
+  const { bookingTime, bookingRate } = props;
+
+  const { date, startTime, endTime } = bookingTime;
+
+  return startTime && endTime ? (
+    <div className={css.bookingTime}>
+      <div className={css.spread}>
+        <h3 className={css.summaryDate}>{date}</h3>
+        <h3 className={css.summaryDate}>${calculateCost(startTime, endTime, bookingRate)}</h3>
+      </div>
+
+      <div className={css.summaryTimeContainer}>
+        <span className={css.summaryTimes}>
+          {startTime} - {endTime}
+        </span>
+        <p className={css.tinyNoMargin}>({calculateTimeBetween(startTime, endTime)} hours)</p>
+      </div>
+    </div>
+  ) : null;
+};
+
+const RefundBookingItem = props => {
+  const { lineItem } = props;
+
+  const { startTime, endTime, date, amount } = lineItem;
+  const formattedDate = moment(date).format('ddd, MM/DD');
+
+  const differenceInHours = addTimeToStartOfDay(date, startTime) - moment().toDate();
+  const isInFuture = differenceInHours > 0;
+  const isFifty = differenceInHours < 48 * 36e5 && isInFuture;
+  const refund = isFifty ? 0.5 : 1;
+
+  return startTime && endTime && isInFuture ? (
+    <div className={css.bookingTime}>
+      <div className={css.spread}>
+        <h3 className={css.summaryDate}>{formattedDate}</h3>
+        <h3 className={css.summaryDate}>
+          ${parseFloat(amount * refund).toFixed(2)} {isFifty ? '(50%)' : '(100%)'}
+        </h3>
+      </div>
+      <div className={css.summaryTimeContainer}>
+        <span className={css.summaryTimes}>
+          {startTime} - {endTime}
+        </span>
+        <p className={css.tinyNoMargin}>({calculateTimeBetween(startTime, endTime)} hours)</p>
+      </div>
+    </div>
+  ) : null;
+};
+
+const RecurringBookingItem = props => {
+  const { bookingRate, weekday, weekdayKey, startDate, showFullWeek } = props;
+
+  const bookingDate = moment(startDate).weekday(weekdayMap[weekdayKey]);
+  const format = showFullWeek ? 'dddd' : 'ddd, MMM Do';
+  const formattedBookingDate = bookingDate.format(format);
+
+  const { startTime, endTime } = weekday[0];
+
+  return (
+    <div className={css.bookingTime}>
+      <div className={css.spread}>
+        <h3 className={css.summaryDate}>{formattedBookingDate}</h3>
+        <h3 className={css.summaryDate}>${calculateCost(startTime, endTime, bookingRate)} </h3>
+      </div>
+      <div className={css.summaryTimeContainer}>
+        <span className={css.summaryTimes}>
+          {startTime} - {endTime}
+        </span>
+        <p className={css.tinyNoMargin}>({calculateTimeBetween(startTime, endTime)} hours)</p>
+      </div>
+    </div>
+  );
+};
+
+const BookingItems = props => {
+  const {
+    bookingDates,
+    selectedBookingTimes,
+    bookingRate,
+    weekdays,
+    startDate,
+    showFullWeek,
+    itemType,
+    lineItems,
+  } = props;
+
+  switch (itemType) {
+    case 'single': {
+      return bookingDates?.map((bookingDate, index) => {
+        const month = new Date(bookingDate).getMonth() + 1;
+        const day = new Date(bookingDate).getDate();
+        const bookingTime = selectedBookingTimes.find(b => b.date === `${month}/${day}`) ?? {};
+
+        return (
+          <BookingItem
+            bookingTime={bookingTime}
+            selectedBookingTimes={selectedBookingTimes}
+            bookingRate={bookingRate}
+            key={bookingTime.date}
+          />
+        );
+      });
+    }
+    case 'refund': {
+      return lineItems.map((lineItem, index) => (
+        <RefundBookingItem lineItem={lineItem} key={index} />
+      ));
+    }
+    case 'recurring': {
+      return Object.keys(weekdays)?.map((weekdayKey, index) => {
+        const weekday = weekdays[weekdayKey];
+
+        return (
+          <RecurringBookingItem
+            weekday={weekday}
+            weekdayKey={weekdayKey}
+            bookingRate={bookingRate}
+            startDate={startDate}
+            showFullWeek={showFullWeek}
+            key={weekdayKey}
+          />
+        );
+      });
+    }
+    default: {
+      return null;
+    }
+  }
+};
+
+export default BookingItems;
--- src/components/BookingSummaryCard/BookingSummaryCard.js
+++ src/components/BookingSummaryCard/BookingSummaryCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
+import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react';
 
 import { Form as FinalForm } from 'react-final-form';
 import {
@@ -9,15 +9,27 @@ import {
   Form,
   FieldRangeSlider,
   Button,
+  ButtonTabNavHorizontal,
 } from '../../components';
 import classNames from 'classnames';
 import { useMediaQuery } from '@mui/material';
 import { convertTimeFrom12to24, calculateProcessingFee } from '../../util/data';
-import { v4 as uuidv4 } from 'uuid';
+import BookingItems from './BookingItems';
+import moment from 'moment';
+import { addTimeToStartOfDay } from '../../util/dates';
 
 import css from './BookingSummaryCard.module.css';
 
 const TRANSACTION_FEE = 0.05;
+const weekdayMap = {
+  sun: 0,
+  mon: 1,
+  tue: 2,
+  wed: 3,
+  thu: 4,
+  fri: 5,
+  sat: 6,
+};
 
 const calculateTimeBetween = (bookingStart, bookingEnd) => {
   const start = convertTimeFrom12to24(bookingStart).split(':')[0];
@@ -34,27 +46,40 @@ const calculateTotalHours = bookingTimes =>
     0
   );
 
-const calculateCost = (bookingStart, bookingEnd, price) =>
-  parseFloat(calculateTimeBetween(bookingStart, bookingEnd) * Number(price)).toFixed(2);
+const calculateRecurringTotalHours = weekdays =>
+  Object.keys(weekdays)?.reduce(
+    (acc, curr) =>
+      acc +
+      (weekdays[curr]?.[0].startTime && weekdays[curr]?.[0].endTime
+        ? calculateTimeBetween(weekdays[curr]?.[0].startTime, weekdays[curr]?.[0].endTime)
+        : 0),
+    0
+  );
 
-const calculateTransactionFee = subTotal =>
-  parseFloat(Number(subTotal) * TRANSACTION_FEE).toFixed(2);
+const calculateBookingFee = subTotal => parseFloat(Number(subTotal) * TRANSACTION_FEE).toFixed(2);
 
-const calculateSubTotal = (bookingTimes, bookingRate) =>
-  bookingTimes
-    ?.reduce(
-      (acc, curr) =>
-        acc +
-        (curr.startTime && curr.endTime
-          ? Number(calculateCost(curr.startTime, curr.endTime, bookingRate))
-          : 0),
-      0
-    )
-    .toFixed(2);
+const calculateSubTotal = (totalHours, bookingRate, lineItems) => {
+  if (lineItems?.length > 0) {
+    return parseFloat(
+      lineItems.reduce((acc, lineItem) => {
+        const { startTime, date, amount } = lineItem;
 
-const calculateTotalCost = (subTotal, transactionFee, processingFee, refundAmount = 0) =>
+        const differenceInHours = addTimeToStartOfDay(date, startTime) - moment().toDate();
+        const isInFuture = differenceInHours > 0;
+        const isFifty = differenceInHours < 48 * 36e5 && isInFuture;
+        const refund = isFifty ? 0.5 : 1;
+
+        return acc + refund * amount;
+      }, 0)
+    ).toFixed(2);
+  }
+
+  return parseFloat(totalHours * Number(bookingRate)).toFixed(2);
+};
+
+const calculateTotalCost = (subTotal, bookingFee, processingFee, refundAmount = 0) =>
   parseFloat(
-    Number(subTotal) + Number(transactionFee) + Number(processingFee) - Number(refundAmount)
+    Number(subTotal) + Number(bookingFee) + Number(processingFee) - Number(refundAmount)
   ).toFixed(2);
 
 const BookingSummaryCard = props => {
@@ -74,22 +99,41 @@ const BookingSummaryCard = props => {
     hideRatesButton,
     hideFees,
     refundAmount,
+    weekdays = [],
+    startDate,
+    endDate,
+    lineItems: allLineItems,
   } = props;
-  const totalHours = calculateTotalHours(selectedBookingTimes);
 
   const isLarge = useMediaQuery('(min-width:1024px)');
   const [showArrow, setShowArrow] = useState(false);
   const [isChangeRatesModalOpen, setIsChangeRatesModalOpen] = useState(false);
+  const [showFullWeek, setShowFullWeek] = useState(false);
 
   const heightRef = useRef(null);
-  const clientHeight = heightRef.current?.clientHeight;
-  const scrollHeight = heightRef.current?.scrollHeight;
 
-  useEffect(() => {
-    if (scrollHeight > clientHeight) {
-      setShowArrow(true);
-    }
-  }, [clientHeight, scrollHeight]);
+  const lineItems = allLineItems?.filter(lineItem => {
+    const { startTime, date } = lineItem;
+
+    const differenceInHours = addTimeToStartOfDay(date, startTime) - moment().toDate();
+    const isInFuture = differenceInHours > 0;
+
+    return isInFuture;
+  });
+
+  const filteredWeekdays = useMemo(() => {
+    const keys = Object.keys(weekdays);
+
+    if (!keys.length) return {};
+
+    return Object.keys(weekdays)?.reduce((acc, weekdayKey) => {
+      const bookingDate = moment(startDate).weekday(weekdayMap[weekdayKey]);
+
+      return bookingDate >= startDate || showFullWeek
+        ? { ...acc, [weekdayKey]: weekdays[weekdayKey] }
+        : acc;
+    }, {});
+  }, [weekdays, startDate, showFullWeek]);
 
   const cardRef = useCallback(node => {
     if (node !== null && window.innerWidth >= 1024) {
@@ -98,15 +142,61 @@ const BookingSummaryCard = props => {
         const isTop = node.scrollTop === 0;
         setShowArrow(isTop);
       });
+
+      const resizeObserver = new ResizeObserver(() => {
+        const isTop = node.scrollTop === 0;
+        setShowArrow(isTop);
+      });
+      resizeObserver.observe(node);
     }
   }, []);
 
-  const subTotal = calculateSubTotal(selectedBookingTimes, bookingRate);
-  const transactionFee = hideFees ? 0 : calculateTransactionFee(subTotal);
-  const processingFee = calculateProcessingFee(subTotal, transactionFee, selectedPaymentMethod);
+  const scrollToBottom = () => {
+    if (heightRef.current) {
+      // Smooth scroll to bottom
+      heightRef.current.scrollTo({
+        top: heightRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  let itemType = 'single';
+
+  if (lineItems && lineItems.length > 0) {
+    itemType = 'refund';
+  } else if (filteredWeekdays && Object.keys(filteredWeekdays).length > 0) {
+    itemType = 'recurring';
+  }
+
+  const totalHours = selectedBookingTimes
+    ? calculateTotalHours(selectedBookingTimes)
+    : Object.keys(filteredWeekdays)?.length > 0
+    ? calculateRecurringTotalHours(filteredWeekdays)
+    : lineItems?.length > 0
+    ? calculateTotalHours(lineItems)
+    : 0;
+
+  const subTotal = calculateSubTotal(totalHours, bookingRate, lineItems);
+  const bookingFee = hideFees ? 0 : calculateBookingFee(subTotal);
+  const processingFee =
+    itemType === 'refund' ? 0 : calculateProcessingFee(subTotal, bookingFee, selectedPaymentMethod);
   const total = hideFees
     ? subTotal
-    : calculateTotalCost(subTotal, transactionFee, processingFee, refundAmount);
+    : calculateTotalCost(subTotal, bookingFee, processingFee, refundAmount);
+
+  const recurringTabs = [
+    {
+      text: 'First Week',
+      selected: !showFullWeek,
+      onClick: () => setShowFullWeek(false),
+    },
+    {
+      text: 'Full Week',
+      selected: showFullWeek,
+      onClick: () => setShowFullWeek(true),
+    },
+  ];
 
   return (
     <div
@@ -116,7 +206,7 @@ const BookingSummaryCard = props => {
       )}
       ref={cardRef}
     >
-      {!hideAvatar ? (
+      {!hideAvatar && itemType !== 'refund' ? (
         <div className={css.cardAvatarWrapper}>
           <Avatar
             user={currentAuthor}
@@ -131,73 +221,104 @@ const BookingSummaryCard = props => {
           </div>
         </div>
       ) : null}
+      {itemType === 'recurring' ? (
+        <>
+          <div className={css.startEnd}>
+            <p>Start Date: {moment(startDate).format('MM/DD/YYYY')}</p>
+            {endDate ? <p>End Date: {moment(endDate).format('MM/DD/YYYY')}</p> : null}
+          </div>
+          <ButtonTabNavHorizontal
+            className={css.tabNav}
+            tabNavClassName={css.tabNav}
+            tabs={recurringTabs}
+          />
+        </>
+      ) : null}
       <div className={css.summaryDetailsContainer}>
         <div className={css.detailsHeadings}>
-          <h2 className={css.detailsTitle}>{subHeading || 'Booking Summary'}</h2>
+          <h2 className={css.detailsTitle}>
+            {subHeading || `${itemType === 'refund' ? 'Refund' : 'Booking'} Summary`}
+          </h2>
         </div>
         <div className={css.bookingTimes}>
-          {bookingDates?.map((bookingDate, index) => {
-            const month = new Date(bookingDate).getMonth() + 1;
-            const day = new Date(bookingDate).getDate();
-            const bookingTime = selectedBookingTimes.find(b => b.date === `${month}/${day}`) ?? {};
-            const date = bookingTime.date;
-            const startTime = bookingTime.startTime;
-            const endTime = bookingTime.endTime;
-
-            return startTime && endTime ? (
-              <div className={css.bookingTime} key={uuidv4()}>
-                <h3 className={css.summaryDate}>
-                  {date} - ${calculateCost(startTime, endTime, bookingRate)}{' '}
-                </h3>
-                <div className={css.summaryTimeContainer}>
-                  <span className={css.summaryTimes}>
-                    {startTime} - {endTime}
-                  </span>
-                  <p className={css.tinyNoMargin}>
-                    ({calculateTimeBetween(startTime, endTime)} hours)
-                  </p>
-                </div>
-              </div>
-            ) : null;
-          })}
+          <BookingItems
+            bookingDates={bookingDates}
+            selectedBookingTimes={selectedBookingTimes}
+            bookingRate={bookingRate}
+            weekdays={filteredWeekdays}
+            startDate={startDate}
+            showFullWeek={showFullWeek}
+            itemType={itemType}
+            lineItems={lineItems}
+          />
         </div>
         <div className={css.totalContainer}>
           {totalHours ? (
             <div className={css.totalCalc}>
-              <h4 className={css.paymentCalc}>
-                <span style={{ paddingRight: '1rem' }}>
-                  {totalHours} hours x ${bookingRate} - ${subTotal}
-                </span>
-                {!hideRatesButton ? (
-                  <InlineTextButton
-                    className={css.changeRateButton}
-                    onClick={() => setIsChangeRatesModalOpen(true)}
-                    type="button"
-                  >
-                    Change Hourly Rate
-                  </InlineTextButton>
-                ) : null}
-              </h4>
+              {itemType !== 'refund' ? (
+                <div className={css.spread}>
+                  <h4 className={css.paymentCalc}>
+                    <span style={{ paddingRight: '0.5rem' }}>
+                      {totalHours} hours x ${bookingRate}
+                    </span>
+                    {!hideRatesButton ? (
+                      <InlineTextButton
+                        className={css.changeRateButton}
+                        onClick={() => setIsChangeRatesModalOpen(true)}
+                        type="button"
+                      >
+                        Change Hourly Rate
+                      </InlineTextButton>
+                    ) : null}
+                  </h4>
+
+                  <h4 className={css.paymentCalc}>${subTotal}</h4>
+                </div>
+              ) : null}
               {!hideFees ? (
                 <>
-                  <h4 className={css.paymentCalc}>CareVine Service fee - ${transactionFee}</h4>
-                  <h4 className={css.paymentCalc}>Processing fee - ${processingFee}</h4>
-                  {refundAmount && refundAmount > 0 ? (
-                    <h4 className={css.paymentCalc} style={{ color: 'var(--failColor)' }}>
-                      Refunded - ${refundAmount}
+                  <div className={css.spread}>
+                    <h4 className={css.paymentCalc}>
+                      CareVine Service Fee {itemType === 'refund' && 'Refund'}
                     </h4>
+                    <h4 className={css.paymentCalc}>${bookingFee}</h4>
+                  </div>
+                  {itemType !== 'refund' ? (
+                    <div className={css.spread}>
+                      <h4 className={css.paymentCalc}>Processing Fee</h4>
+                      <h4 className={css.paymentCalc}>${processingFee}</h4>
+                    </div>
+                  ) : null}
+                  {refundAmount && refundAmount > 0 ? (
+                    <div className={css.spread}>
+                      <h4 className={css.paymentCalc} style={{ color: 'var(--failColor)' }}>
+                        Refunded
+                      </h4>
+                      <h4 className={css.paymentCalc} style={{ color: 'var(--failColor)' }}>
+                        ${refundAmount}
+                      </h4>
+                    </div>
                   ) : null}
                 </>
               ) : null}
             </div>
           ) : null}
-          <h3 className={css.total}>Total: ${total}</h3>
+          <div className={css.spread}>
+            <h3 className={css.total}>Total {itemType === 'refund' && 'Refund'}:</h3>
+            <h3 className={css.total}>${total}</h3>
+          </div>
         </div>
       </div>
       {showArrow ? (
-        <IconArrowHead direction="down" height="2em" width="2em" className={css.arrow} />
+        <IconArrowHead
+          direction="down"
+          height="1.5em"
+          width="1.5em"
+          className={css.arrow}
+          onClick={scrollToBottom}
+        />
       ) : null}
-      {!hideRatesButton ? (
+      {!hideRatesButton && itemType !== 'refund' ? (
         <Modal
           id="changeRatesModal"
           isOpen={isChangeRatesModalOpen}
