df7dd92555ef694267d0fe289d92ecbcadb4a43e
peytonhobson <peyton.hobson1@gmail.com>
2023-08-05 08:58:36 -0600

    Created section mobile steps

--- src/containers/LandingPage/sections/index.js
+++ src/containers/LandingPage/sections/index.js
@@ -3,3 +3,4 @@ export { default as SectionEmployer } from './SectionEmployer/SectionEmployer';
 export { default as SectionCaregiver } from './SectionCaregiver/SectionCaregiver';
 export { default as SectionCity } from './SectionCity/SectionCity';
 export { default as SectionBlog } from './SectionBlog/SectionBlog';
+export { default as SectionMobileSteps } from './SectionMobileSteps/SectionMobileSteps';
