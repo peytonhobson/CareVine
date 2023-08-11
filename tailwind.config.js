89b84147f5c674a193aa37c5afd46bf7dbfb9cee
peytonhobson <peyton.hobson1@gmail.com>
2023-08-10 20:12:03 -0600

    Integrated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/containers/**/*.{html,js}',
+    './src/components/**/*.{html,js}',
+    './public/index.html',
+  ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     screens: {
       sm: '550px',
@@ -13,6 +20,29 @@ module.exports = {
       // => @media (min-width: 1280px) { ... }
       lgwp: '1128px',
       // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: '#6ba0b6',
+        'primary-light': '#a6c6d3',
+        'primary-dark': '#2f5260',
+        secondary: '#77a08a',
+        'secondary-light': '#74ad8a',
+        'secondary-dark': '#395244',
+        success: '#2ecc71',
+        'success-dark': '#239954',
+        'success-light': '#f0fff6',
+        error: '#ff0000',
+        'error-light': '#fff0f0',
+        attention: '#ffaa00',
+        'attention-light': '#fff7f0',
+        'matter-dark': '#000000',
+        matter: '#4a4a4a',
+        anti: '#b2b2b2',
+        negative: '#e7e7e7',
+        bright: '#fcfcfc',
+        light: '#ffffff',
+      },
     },
   },
   plugins: [
