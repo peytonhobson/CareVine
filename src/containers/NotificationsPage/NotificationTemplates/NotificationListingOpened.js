47b60317445b5fad6af0e8303ceff8342307e03a
peytonhobson <peyton.hobson1@gmail.com>
2023-09-06 16:55:42 -0600

    Changed listing opened and removed notifications

--- src/containers/NotificationsPage/NotificationTemplates/NotificationListingOpened.js
+++ src/containers/NotificationsPage/NotificationTemplates/NotificationListingOpened.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { createSlug } from '../../../util/urlHelpers';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+import Card from '@material-ui/core/Card';
 import { NamedLink } from '../../../components';
 import { userDisplayNameAsString } from '../../../util/data';
 
 import css from './NotificationTemplates.module.css';
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    borderRadius: 'var(--borderRadius)',
+    transition: 'all 0.2s ease-in 0s !important',
+    padding: '3rem',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '35rem',
+    },
+  },
+}));
+
 const NotificationListingOpened = props => {
   const { currentUser, listing } = props;
 
+  const classes = useStyles();
+
   return (
     <div className={css.root}>
-      <h1 className={css.title}>Success!</h1>
-      <p className={css.message}>
-        Your profile listing has been posted on the marketplace and is now viewable by others.
-      </p>
-      <NamedLink
-        className={css.linkButton}
-        name="ListingPage"
-        params={{
-          slug: createSlug(userDisplayNameAsString(currentUser)),
-          id: listing?.id.uuid ?? 'string',
-        }}
-      >
-        <span>View Listing</span>
-      </NamedLink>
+      <Card className={classes.card}>
+        <h1 className={css.title}>Success!</h1>
+        <p className={css.message}>
+          Your profile listing has been posted on the marketplace and is now viewable by others.
+        </p>
+        <NamedLink
+          className={css.linkButton}
+          name="ListingPage"
+          params={{
+            slug: createSlug(userDisplayNameAsString(currentUser)),
+            id: listing?.id.uuid ?? 'string',
+          }}
+        >
+          <span>View Listing</span>
+        </NamedLink>
+      </Card>
     </div>
   );
 };
